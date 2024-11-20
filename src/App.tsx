1e3c7d69a22efdd5c2563fe7552497719aa0783e
cadenjackson11 <143419483+cadenjackson11@users.noreply.github.com>
2024-11-20 15:18:47 -0600

    modal complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
 import {
   createHashRouter,
@@ -156,7 +156,9 @@ const App = () => {
 
   const [darkMode, setDarkMode] = useState(false);
 
+  const emailModalRef = useRef<HTMLDialogElement | null>(null);
 
+  //special react hook use last
   useEffect(
     () => {
       
@@ -229,12 +231,14 @@ const App = () => {
         className="navbar bg-base-200"
       >
         <span 
-          className="text-4xl m-auto font-bold">
+          className="text-4xl ml-1 font-bold">
             { title }
         </span>
 
         <div className="flex gap-1 ml-auto">
-          <button className='btn'>
+          <button 
+            className='btn'
+            onClick={() => emailModalRef.current?.showModal()}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
               <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12a7.5 7.5 0 0 0 15 0m-15 0a7.5 7.5 0 1 1 15 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077 1.41-.513m14.095-5.13 1.41-.513M5.106 17.785l1.15-.964m11.49-9.642 1.149-.964M7.501 19.795l.75-1.3m7.5-12.99.75-1.3m-6.063 16.658.26-1.477m2.605-14.772.26-1.477m0 17.726-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205 12 12m6.894 5.785-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495" />
               </svg>
@@ -279,6 +283,22 @@ const App = () => {
           router={myRouter}
         />
       </div>
+
+
+      <dialog 
+        ref={emailModalRef}
+        className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box w-11/12 max-w-5xl">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Press ESC key or click the button below to close</p>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+
     </div>
   );
 }
